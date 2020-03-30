7041ab8760b1f601dcdda0d3619e87ac4d4d00f1
CJ Liu <36778377+hiCJLiu@users.noreply.github.com>
2020-03-29 19:01:08 -0700

    Add files via upload

--- 5g_game.py
+++ 5g_game.py
@@ -2,8 +2,10 @@
 
 import tkinter as tk     
 import random
+import math
 
 time = 0
+speed_list = [0,0,0,0,0]
 class People:
 
     def __init__(self):
@@ -42,11 +44,12 @@ class MyCanvas(tk.Canvas):
         self.pack()
 
         self.people = []   
-        self.bs = []      
-        for _ in range(5):
+        self.bs = []
+        self.diction ={0:"red",1:"green", 2:"blue", 3: "brown", 4: "yellow"}      
+        for i in range(5):
             people = People()
             self.people.append(people)
-            self.bs.append(self.create_oval(people.xpos - 10, people.ypos - 10, people.xpos + 10, people.ypos + 10, fill="brown"))
+            self.bs.append(self.create_oval(people.xpos - 10, people.ypos - 10, people.xpos + 10, people.ypos + 10, fill=self.diction[i]))
 
         # self.create_polygon(400,400,430, 400, 400, 430,430, 430)
         # self.create_polygon(280,280,310, 280, 280, 310,310, 310)
@@ -57,19 +60,45 @@ class MyCanvas(tk.Canvas):
         	self.tower_object.append(tow)
 
         #self.create_polygon(400,400,430, 400, 400, 430,430, 430)
+        # lst =[]
+        #     lst.append(obs.start_x, obs.start_y, obs.start_x + obs.length, obs.start_y + obs.breadth)
+        #     self.obstacle_angle.append(lst)
         self.obstacle_object = []
-
+        self.obstacle_angle = []
         for _ in range(6):
-        	obs = Obstacle()
-        	self.create_rectangle(obs.start_x, obs.start_y, obs.start_x + obs.length, obs.start_y + obs.breadth, outline="#f11", fill="blue", width='1')
-        	self.obstacle_object.append(obs)
+            obs = Obstacle()
+            self.create_rectangle(obs.start_x, obs.start_y, obs.start_x + obs.length, obs.start_y + obs.breadth, outline="#f11", fill="blue", width='1')
+            self.obstacle_object.append(obs)
+            lst = []
+            lst.append(obs.start_x)
+            lst.append(obs.start_x)
+            lst.append(obs.start_x + obs.length)
+            lst.append(obs.start_y + obs.breadth)
+            self.obstacle_angle.append(lst)
 
-       
+        # for _ in range(6):
+
+        # 	obs = Obstacle()
+        # 	self.create_rectangle(obs.start_x, obs.start_y, obs.start_x + obs.length, obs.start_y + obs.breadth, outline="#f11", fill="blue", width='1')
+        # 	self.obstacle_object.append(obs)
+
         self.create_text(100,10,fill="darkblue",font="Times 20 italic bold",text="5G Simulation")
         self.create_text(550,10,fill="black",font="Times 10 italic bold",text="Brown Circles: People")
         self.create_text(700,10,fill="black",font="Times 10 italic bold",text="Blue rectangles: Obstacles")
         self.create_text(370,15,fill="black",font="Times 15 italic bold",text="Seconds")
+        self.y_cord = 700
+        x_cord = 60
+        self.x_cord_speed = 100
+        step = 20
+        self.speed_id_list =[]
+        for i in range(len(self.people)):
+            # self.create_text(x_cord + i*80,y_cord,fill="black",font="Times 15 italic bold",text="Gbps")
+            self.create_oval(x_cord + i*80 - 10, self.y_cord - 10, x_cord + i*80 + 10, self.y_cord + 10, fill=self.diction[i]) 
+            self.speed_id_list.append(self.create_text(self.x_cord_speed + i*80, self.y_cord,fill="black",font="Times 15 italic bold",text= str(00.00)))
+
+
         
+
         for i in range(len(self.tower_object)):
             x_cord_1 = self.tower_object[i].first + 15
             y_cord_1 = self.tower_object[i].first + 15
@@ -78,7 +107,10 @@ class MyCanvas(tk.Canvas):
             self.create_oval(x_cord_1-200, y_cord_1 - 200, x_cord_1+ 200, y_cord_1+200)
             self.create_text(x_cord_1, y_cord_1 - 180, fill="black",font="Times 12 italic",text="5 Gbps")
             self.create_oval(x_cord_1-300, y_cord_1 - 300, x_cord_1+ 300, y_cord_1+300)
-            self.create_text(x_cord_1, y_cord_1 - 280, fill="black",font="Times 12 italic",text="1 Gbps")
+            self.create_text(x_cord_1, y_cord_1 - 280, fill="black",font="Times 12 italic",text="3 Gbps")
+
+        self.circle_first = self.tower_object[0].first + 15
+        self.circle_second = self.tower_object[1].first + 15
 
 
 
@@ -87,11 +119,43 @@ class MyCanvas(tk.Canvas):
         self.time_id = self.create_text(300,15,fill="black",font="Times 15 italic bold",text= str(00.00))
         self.run()
 
+    def get_speed(self, x, y, centre):
+
+        if((x - centre) * (x - centre) + (y - centre) * (y - centre) <= 50*50):
+            return 10
+        elif ((x - centre) * (x - centre) + (y - centre) * (y - centre) <= 200*200):
+            return 5
+        elif ((x - centre) * (x - centre) + (y - centre) * (y - centre) <= 300*300):
+            return 3
+        else:
+            return 1
+            
+            
+    def find_blocking_status(self, x, y, centre):
+        ball_angle = math.atan2(x-centre, y- centre) * 180/math.pi
+        ball_dist = (x- centre) * (x - centre) + (y - centre) * (y - centre)
+        for i in range(len(self.obstacle_angle)):
+            angle_1 = math.atan2(self.obstacle_angle[i][0]-centre, self.obstacle_angle[i][1]- centre) * 180/math.pi
+            angle_2 = math.atan2(self.obstacle_angle[i][2]-centre, self.obstacle_angle[i][3]- centre) * 180/math.pi
+            tower_x =  (self.obstacle_angle[i][0] + self.obstacle_angle[i][2])/ 2
+            tower_y =  (self.obstacle_angle[i][1] + self.obstacle_angle[i][3])/ 2
+            tower_dist = (tower_x- centre) * (tower_x- centre) + (tower_y - centre) * (tower_y - centre)
+            if(angle_1 >= angle_2):
+                if(ball_angle>= angle_2 and ball_angle <= angle_1 and tower_dist <= ball_dist):
+                    return False
+            else:
+                if(ball_angle>= angle_1 and ball_angle <= angle_2 and tower_dist <= ball_dist):
+                    return False
+        return True
+
+
     def run(self):
         global time
         time +=0.01
         self.delete(self.time_id)
+
         self.time_id = self.create_text(300,15,fill="black",font="Times 15 italic bold",text= str(round(time,2)))
+        count=0
         for p, people in zip(self.bs, self.people):
             self.move(p, people.xspeed, people.yspeed)
             pos = self.coords(p)
@@ -113,8 +177,22 @@ class MyCanvas(tk.Canvas):
             	x_cord_2 = x_cord_1 + 50
             	y_cord_2 = y_cord_1 + 50
             	if(pos[0] >= x_cord_1  and pos[0] <= x_cord_2  and pos[1] >= y_cord_1  and pos[1] <= y_cord_2 ):
-            		people.xspeed = - people.xspeed            		
+            		people.xspeed = - people.xspeed 
 
+            self.delete(self.speed_id_list[count])
+            speed_1 = self.get_speed(pos[0],pos[1], self.circle_first)
+            speed_2 = self.get_speed(pos[0], pos[1], self.circle_second)
+            
+            blocking_1 = self.find_blocking_status(pos[0],pos[1], self.circle_first)
+            if(blocking_1 == False):
+                speed_1 = 0
+            blocking_2 = self.find_blocking_status(pos[0],pos[1], self.circle_second)
+            if(blocking_2 == False):
+                speed_2 = 0
+
+            speed_final = (speed_1 + speed_2)/2
+            self.speed_id_list[count] = self.create_text(self.x_cord_speed + count*80, self.y_cord,fill="black",font="Times 15 italic bold",text= str(round(speed_final,2)))   
+            count+=1
 
         self.after(10, self.run)
         
